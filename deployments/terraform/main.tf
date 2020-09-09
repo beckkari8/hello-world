460ebb39b3e7a47563f280787855ada82bcb981d
BEKTUR KARIMOV <beckkari8@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-09-09 06:51:45 +0000

    added sen-env.sh

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,36 @@
+ module "academy-deploy" {
+   source  = "fuchicorp/chart/helm"
+   deployment_name        = "hello-world"
+   deployment_environment = "${var.deployment_environment}"
+   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+   deployment_path        = "hello-world"
+   template_custom_vars  = {     
+     deployment_image     = "${var.deployment_image}"
+   }
+ }
+output "application_endpoint" {
+    value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+}
+variable  "deployment_image" {
+    default = "docker.beckinc.org/hello-world-job-dev-feature:adb2357"
+}
+variable "deployment_environment" {
+    default = "stage"
+}
+variable "deployment_endpoint" {
+    type = "map"
+     default = {
+        test  = "test.hello"
+        dev  = "dev.hello"
+        qa   = "qa.hello"
+        prod = "hello"
+        stage = "stage.hello"
+  }
+}
+variable "google_domain_name" {
+    default = "fuchicorp.com"
+}
+
+variable "google_bucket_name" {
+    default = "beck-fuchicorp"
+}
